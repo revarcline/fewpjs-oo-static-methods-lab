20b674ae247341ce750fdd39535ad53591d90a99
Alex Cline <rev.a.r.cline@gmail.com>
2021-03-31 12:04:36 -0500

    commit tree d0460f7a08b9a9a1f83d38cdb3d56ab6426c9980

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.slice(0, 1).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^a-z0-9\-\s']/gi, "");
+  }
+
+  static titleize(string) {
+    const lowers = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from",
+    ];
+    const list = string.split(" ");
+    const first = Formatter.capitalize(list.shift());
+
+    let out = list.map((word) => {
+      if (lowers.includes(word)) {
+        return word.toLowerCase();
+      } else {
+        return Formatter.capitalize(word);
+      }
+    });
+
+    out.unshift(first);
+    return out.join(" ");
+  }
 }
